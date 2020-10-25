dfed2c1007df52b85b5189d3cb6502629883dfa7
Gilso Manfredi <gilso.manfredi@gmail.com>
2020-10-25 00:19:27 -0300

    Endpoint source com o link do projeto

---
+++ src/main/java/io/github/gilsomanfredi/cadastropessoa/controller/source/SourceController.java
@@ -0,0 +1,17 @@
+package io.github.gilsomanfredi.cadastropessoa.controller.source;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/source")
+public class SourceController {
+
+    @GetMapping
+    public String source() {
+
+        return "https://github.com/gilsomanfredi/desafio-tecnico-softplan";
+    }
+
+}
